a83eddc61dc288c2a39b82fff760beba5fa9f0dc
Phasin Sae-ngow <bird.biob3@gmail.com>
2023-11-22 08:17:00 +0700

    rewrite ball in oop

--- ball.py
+++ ball.py
@@ -1,35 +1,52 @@
-import turtle
-import random
+import turtle, random
 
-def draw_circle(color, size, x, y):
-    # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
+class Ball:
+    def __init__(self, color, size, x, y):
+        self.__color = color
+        self.__size = size
+        self.__x = x
+        self.__y = y
 
-def move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-    # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]
-    ypos[i] += vy[i]
+    @property
+    def color(self):
+        return self.__color
 
-    # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i] + vx[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
+    @property
+    def size(self):
+        return self.__size
 
-    # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i] + vy[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
+    @property
+    def x(self):
+        return self.__x
 
-def initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls):
-    # create random number of balls, num_balls, located at random positions within the canvas; each ball has a random velocity value in the x and y direction and is painted with a random color
-    for i in range(num_balls):
-        xpos.append(random.randint(-1*canvas_width + ball_radius, canvas_width - ball_radius))
-        ypos.append(random.randint(-1*canvas_height + ball_radius, canvas_height - ball_radius))
-        vx.append(random.randint(1, 0.01*canvas_width))
-        vy.append(random.randint(1, 0.01*canvas_height))
-        ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def x_velo(self):
+        return self.x_velo
+
+    @property
+    def y_velo(self):
+        return self.y_velo
+    
+    def draw_circle(self):
+        turtle.penup()
+        turtle.color(self.__color)
+        turtle.fillcolor(self.__color)
+        turtle.goto(self.__x, self.__y)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.__size)
+        turtle.end_fill()
+    
+    def move_circle(self, canvas_width, canvas_height):
+        __vx = random.randint(1, 0.01*canvas_width)
+        __vy = random.randint(1, 0.01*canvas_height)
+        self.__x += __vx
+        self.__y += __vy
+        if abs(self.__x + __vx) > (canvas_width - self.__size):
+            __vx = -__vx
+        if abs(self.__y + __vy) > (canvas_height - self.__size):
+            __vy = -__vy
